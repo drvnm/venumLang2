47b97c3227116248c57c185841199f82ae9a78ad
darealvenum <marawanhoi@hotmail.com>
2022-04-23 14:05:22 +0200

    started lox

---
+++ lox/value.h
@@ -0,0 +1,20 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double Value;
+typedef struct
+{
+    int capacity;
+    int count;
+    Value *values;
+} ValueArray;
+
+// functions to dynamically allocate and free memory
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif
