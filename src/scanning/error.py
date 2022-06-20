0f60a99234df0c8f5eb2345cc4d1f3df849986b5
farkon00 <sammer2016sammer@gmail.com>
2022-06-21 00:05:08 +0300

    Added color to error message

--- src/scanning/error.py
+++ src/scanning/error.py
@@ -5,12 +5,11 @@ from intermediate.tokens import Token
 def error(line: Union[Token, int], message: str):
     if isinstance(line, Token):
         if hasattr(line, 'file'):
-            index = " " * line.col
-            print(f'[FILE] {line.file}:{line.line}:{line.col}')
+            print(f'[FILE] {line.file}:{line.line}:{line.col}', file=sys.stderr)
             print(f'[LINE CONTENT] {line.line_content.strip()}', file=sys.stderr)
-            print(f'[ERROR] {message}', file=sys.stderr)
+            print(f'\033[31;[ERROR]\033[0; {message}', file=sys.stderr)
             sys.exit(1)
 
     line = line.line if hasattr(line, 'line') else line
-    print(f'[line {line}] Error: {message}', file=sys.stderr)
+    print(f'[line {line}] \033[31;1mError\033[0m: {message}', file=sys.stderr)
     sys.exit(1)
